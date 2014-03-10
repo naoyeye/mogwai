af319ff05bd153076dcfffe221cb6b7507572602
J.Y Han <ihanjiyun@gmail.com>
2014-03-10 18:26:03 +0800

    polish style

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -1,4 +1,9 @@
 
 $(function(){
     $('.ui.accordion').accordion();
+
+    $('#switchAsideBtn').click(function(){
+        $('#main').toggleClass('aside_closed');
+        $('#switchAsideBtn i').toggleClass('right')
+    })
 })
